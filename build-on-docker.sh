ff0a6ea88b1dd5c15b909c9a95c4c777d4ab88c7
Kousuke Kawahira <kawahira@binzume.net>
2017-09-22 02:44:59 +0900

    Docker

--- build-on-docker.sh
+++ build-on-docker.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 APP=k-on-server
 GOOS=linux
-docker run --rm -v "$PWD":/usr/src/$APP -v $PWD/build_src:/go/src -w /usr/src/$APP -e CGO_ENABLED=0 -e GOOS=$GOOS golang:1.8 bash -c "go get; go build"
+DIR=$(cd $(dirname $0); pwd)
+
+mkdir -p _go
+docker run --rm -v "$DIR":/usr/src/$APP -v $DIR/_go:/go -w /usr/src/$APP -e CGO_ENABLED=0 -e GOOS=$GOOS golang:1.8 bash -c "go get; go build"
+
+# build image
+cd $DIR
+mkdir -p docker
+cp -r $APP static docker
+docker rmi $APP:latest
+docker build -t $APP:latest docker
 
